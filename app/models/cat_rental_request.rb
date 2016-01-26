34da3f83b3f9993aa3d25daa55665dcb99286368
Ryan Chong <chongr@umich.edu>
2016-01-26 14:51:42 -0800

    add cat rental class

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -4,12 +4,19 @@ class CatRentalRequest < ActiveRecord::Base
     'DENIED']
   validates :cat_id, :start_date, :end_date, :status, presence: true
   validates :status, inclusion: STATUSES
-  #validate :overlapping_approved_requests
+  validate :overlapping_approved_requests
 
   def overlapping_requests
-    CatRentalRequest.where('(start_date BETWEEN #{self.start_date} AND #{self.end_date} OR
-                            end_date BETWEEN #{self.start_date} AND #{self.end_date}) AND
-                            cat_id = #{self.cat_id}')
+    CatRentalRequest.where("(start_date BETWEEN '#{self.start_date}' AND '#{self.end_date}' OR
+                            end_date BETWEEN '#{self.start_date}' AND '#{self.end_date}') AND
+                            cat_id = #{self.cat_id}")
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.delete(self)
+    if overlapping_requests.any? {|req| req.status == "APPROVED"}
+      errors[:status] = "Cat rented during that time period"
+    end
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-Cat.create!({name: 'Snuggles', birth_date: Date.parse('11-04-1991'), color: 'platinum', sex: 'F'})
-Cat.create!({name: 'Mr. Cat', birth_date: Date.parse('08-12-2015'), color: 'black', sex: 'F'})
-Cat.create!({name: 'Grinch', birth_date: Date.parse('10-10-1804'), color: 'green', sex: 'M'})
+Cat.create!({name: 'Snuggles', birth_date: Date.parse('11-04-1991').strftime, color: 'platinum', sex: 'F'})
+Cat.create!({name: 'Mr. Cat', birth_date: Date.parse('08-12-2015').strftime, color: 'black', sex: 'F'})
+Cat.create!({name: 'Grinch', birth_date: Date.parse('10-10-1804').strftime, color: 'green', sex: 'M'})
 
 
-CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('11-11-2015'), end_date: Date.parse('12-12-2015')})
-CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('10-10-2015'), end_date: Date.parse('30-11-2015')})
-CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('20-11-2015'), end_date: Date.parse('01-01-2016')})
-CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('10-10-2014'), end_date: Date.parse('30-11-2014')})
+CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('11-11-2015').strftime, end_date: Date.parse('12-12-2015').strftime})
+CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('10-10-2015').strftime, end_date: Date.parse('30-11-2015').strftime})
+CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('20-11-2015').strftime, end_date: Date.parse('01-01-2016').strftime})
+CatRentalRequest.create!({cat_id: 1, start_date: Date.parse('10-10-2014').strftime, end_date: Date.parse('30-11-2014').strftime})
